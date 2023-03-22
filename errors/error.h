96541a5114aa6f431554eeec2efd902628fc636b
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-03-22 13:13:28 -0300

    Manejo de errores

---
+++ errors/error.h
@@ -0,0 +1,13 @@
+#ifndef ERROR_H_
+#define ERROR_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define __CONDITION_ERROR(cond, pl, s) \
+  if(!cond){ \
+    fprintf(stderr, "Error into %s: %s\n", pl, s); \
+    exit(EXIT_FAILURE); \
+  }
+
+#endif
