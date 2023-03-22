ff412bbea7ad56f5b62ad13e03b47053d0917ffa
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-03-22 13:13:37 -0300

    TAD Vector

---
+++ vector/vector.c
@@ -0,0 +1,217 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h> // memcpy
+
+#include "vector.h"
+#include "../errors/error.h"
+
+// ------------------------ AUXILIAR FUNCTIONS ------------------------
+
+static void *create_element(void *el){
+  __CONDITION_ERROR((el != NULL), "create_element", "Element is NULL");
+
+  void *nw_el = NULL; nw_el = malloc(sizeof(void*));
+  __CONDITION_ERROR((nw_el != NULL), "create_element (static function to copy *el)", "Malloc error");
+
+  nw_el = memcpy(nw_el, el, sizeof(void*));
+  return nw_el;
+}
+
+// ------------------------ STRUCTURE ------------------------
+
+struct VectorSt{
+  size_t size, capacity;
+  void **array;
+};
+
+// ------------------------ CONSTRUCTIVE FUNCTIONS ------------------------
+
+Vector vector_initialize(Vector v){
+  __CONDITION_ERROR((v == NULL), "vector_initialize", "Vector doesn't NULL");
+
+  v = (Vector)malloc(sizeof(struct VectorSt));
+  __CONDITION_ERROR((v != NULL), "vector_initialize", "Malloc error");
+
+  v->size = v->capacity = 0;
+  v->array = NULL;
+
+  __CONDITION_ERROR((v != NULL), "vector_initialize", "Vector is NULL");
+  return v;
+}
+
+void vector_push_back(Vector v, void *el){
+  __CONDITION_ERROR((v != NULL), "vector_push_back", "Vector is NULL");
+  __CONDITION_ERROR((el != NULL), "vector_push_back", "Element is NULL");
+
+  if(v->capacity == 0){
+    v->capacity = 1;
+    
+    v->array = malloc(sizeof(void*));
+    __CONDITION_ERROR((v->array != NULL), "vector_push_back", "Malloc error");
+
+  }else if(v->capacity == v->size){
+    v->capacity *= 2;
+    v->array = realloc(v->array, sizeof(void*) * v->capacity);
+  }
+
+  v->array[v->size] = create_element(el);
+  v->size++;
+}
+
+// ------------------------ DESTRUCTIVE FUNCTIONS ------------------------
+
+void vector_free(Vector v){
+  if((v != NULL)){
+    for(unsigned int i = 0; i < v->size; i++){
+      free(v->array[i]); v->array[i] = NULL;
+    }
+    free(v->array); v->array = NULL;
+    free(v); v = NULL;
+  }
+
+  __CONDITION_ERROR((v == NULL), "vector_free", "Bad free of vector");
+}
+
+// ------------------------ INFORMATIVE FUNCTIONS ------------------------
+
+bool vector_is_empty(Vector v){
+  __CONDITION_ERROR((v != NULL), "vector_is_empty", "Vector is NULL");
+
+  return v->size == 0;
+}
+
+size_t vector_size(Vector v){
+  __CONDITION_ERROR((v != NULL), "vector_size", "Vector is NULL");
+
+  return v->size;
+}
+
+void *vector_element(Vector v, unsigned int idx){
+  __CONDITION_ERROR((v != NULL), "vector_element", "Vector is NULL");
+  __CONDITION_ERROR((idx < vector_size(v)), "vector_element", "Overflow of index");
+
+  void *el = create_element(v->array[idx]);
+
+  __CONDITION_ERROR((el != NULL), "vector_element", "Bad indexed search");
+  return el;
+}
+
+unsigned int vector_search(Vector v, void *el, int(*cmp)(const void *a, const void *b)){
+  __CONDITION_ERROR((v != NULL), "vector_search", "Vector is NULL");
+  __CONDITION_ERROR((el != NULL), "vector_search", "Element is NULL");
+
+  unsigned int idx = v->size;
+  for(unsigned int i = 0; i < v->size; i++){
+    if(cmp(v->array[i], el) == 0){
+      idx = i;
+      break;
+    }
+  }
+
+  __CONDITION_ERROR((idx == vector_size(v) || cmp(v->array[idx], el) == 0), "vector_search", "Bad search");
+  return idx;
+}
+
+unsigned int vector_sorted_search(Vector v, void *el, int(*cmp)(const void *a, const void *b)){
+  __CONDITION_ERROR((v != NULL), "vector_sorted_search", "Vector is NULL");
+  __CONDITION_ERROR((el != NULL), "vector_sorted_search", "Element is NULL");
+
+  unsigned int idx = v->size;
+  unsigned int left = 0, right = v->size;
+  while(left+1 < right){
+    unsigned int middle = (left+right)/2;
+    int cmp_result = cmp(v->array[middle], el);
+    if(cmp_result == 0){
+      idx = middle;
+      break;
+
+    }else if(cmp_result < 0){
+      left = middle;
+
+    }else{
+      right = middle;
+    }
+  }
+  if(cmp(v->array[left], el) == 0){
+    idx = left;
+  }
+
+  __CONDITION_ERROR((idx == vector_size(v) || cmp(v->array[idx], el) == 0), "vector_sorted_search", "Bad search");
+  return idx;
+}
+
+// ------------------------ MODIFICATIVE FUNCTIONS ------------------------
+
+void vector_assign(Vector v, void *el, unsigned int idx){
+  __CONDITION_ERROR((v != NULL), "vector_assign", "Vector is NULL");
+  __CONDITION_ERROR((el != NULL), "vector_assing", "Element is NULL");
+  __CONDITION_ERROR((idx < vector_size(v)), "vector_assign", "Overflow of index");
+
+  void *nw_el = create_element(el);
+  free(v->array[idx]); v->array[idx] = NULL;
+  v->array[idx] = nw_el;
+}
+
+// ------------------------ ERASE FUNCTIONS ------------------------
+
+void vector_pop_back(Vector v){
+  __CONDITION_ERROR((v != NULL), "vector_pop_back", "Vector is NULL");
+  __CONDITION_ERROR((!vector_is_empty(v)), "vector_pop_back", "Vector is empty");
+
+  free(v->array[v->size-1]); v->array[v->size-1] = NULL;
+  v->size--;
+}
+
+void vector_erase(Vector v, unsigned int idx){
+  __CONDITION_ERROR((v != NULL), "vector_erase", "Vector is NULL");
+  __CONDITION_ERROR((!vector_is_empty(v)), "vector_erase", "Vector is empty");
+  __CONDITION_ERROR((idx < vector_size(v)), "vector_erase", "Overflow of index");
+  
+  free(v->array[idx]); v->array[idx] = NULL;
+  for(unsigned int i = idx; i < v->size-1; i++){
+    v->array[i] = v->array[i+1];
+  }
+  v->array[v->size-1] = NULL;
+
+  v->size--;
+}
+
+// ------------------------ SORT FUNCTIONS ------------------------
+
+static void merge_sort(Vector v, int(*cmp)(const void *a, const void *b), unsigned int left, unsigned int right){
+  if(left < right){
+    unsigned int middle = left+(right-left)/2;
+    merge_sort(v, cmp, left, middle);
+    merge_sort(v, cmp, middle+1, right);
+
+    Vector aux = NULL; aux = vector_initialize(aux);
+    unsigned int i = left, j = middle+1;
+    while(i <= middle && j <= right){
+      if(cmp(v->array[i], v->array[j]) <= 0){
+        vector_push_back(aux, v->array[i]); i++;
+      }else{
+        vector_push_back(aux, v->array[j]); j++;
+      }
+    }
+    while(i <= middle){
+      vector_push_back(aux, v->array[i]); i++;
+    }
+    while(j <= right){
+      vector_push_back(aux, v->array[j]); j++;
+    }
+    
+    for(unsigned int idx = 0; idx < aux->size; idx++){
+      vector_assign(v, aux->array[idx], idx+left);
+    }
+
+    vector_free(aux);
+
+    __CONDITION_ERROR((i == middle+1 && j == right+1), "merge_sort", "Bad sorting");
+  }
+}
+
+void vector_merge_sort(Vector v, int(*cmp)(const void *a, const void *b)){
+  __CONDITION_ERROR((v != NULL), "vector_merge_sort", "Vector is NULL");
+
+  merge_sort(v, cmp, 0, v->size-1);
+}
---
+++ vector/vector.h
@@ -0,0 +1,133 @@
+#ifndef DEQUE_H_
+#define DEQUE_H_
+
+// ------------------------ LIBRARIES (USED FOR TYPES) ------------------------
+
+#include <stdio.h>
+#include <stdbool.h>
+
+// ------------------------ STRUCTURE ------------------------
+
+struct VectorSt;
+
+typedef struct VectorSt* Vector;
+
+// ------------------------ CONSTRUCTIVE FUNCTIONS ------------------------
+
+/**
+ * @brief Vector inicialization (create the structure and allocate needed memory)
+ * @param v 
+ * @return Vector initialized
+ * @pre v == NULL
+ * @post v != NULL
+ */
+Vector vector_initialize(Vector v);
+
+/**
+ * @brief Push a element in the end of the vector (the structure generates a copy of the element)
+ * @param v 
+ * @param el 
+ * @pre v != NULL && el != NULL
+ * @post vector_element(v, vector_size(v)-1) == el
+ */
+void vector_push_back(Vector v, void *el);
+
+// ------------------------ DESTRUCTIVE FUNCTIONS ------------------------
+
+/**
+ * @brief Free and erase all the structure and memory used (only local)
+ * @param v 
+ * @post v == NULL
+ */
+void vector_free(Vector v);
+
+// ------------------------ INFORMATIVE FUNCTIONS ------------------------
+
+/**
+ * @brief Check if the vector is empty
+ * @param v 
+ * @return True if the vector is empty (0 elements) and False otherwise
+ * @pre v != NULL
+ */
+bool vector_is_empty(Vector v);
+
+/**
+ * @brief Calculate the size of the vector
+ * @param v 
+ * @return Size of the vector
+ * @pre v != NULL
+ */
+size_t vector_size(Vector v);
+
+/**
+ * @brief Element with index idx into the vector (returns a copy)
+ * @param v 
+ * @param idx 
+ * @return Pointer to a copy of the element
+ * @pre v != NULL && idx < vector_size(v)
+ * @post el != NULL
+ */
+void *vector_element(Vector v, unsigned int idx);
+
+/**
+ * @brief Search with O(N) complexity an element into the vector
+ * @param v 
+ * @param el 
+ * @param cmp
+ * @return Index of the element or vector_size(v) otherwise
+ * @pre v != NULL && el != NULL
+ * @post idx == vector_size(v) || cmp(vector_element(v, idx), el) == 0
+ */
+unsigned int vector_search(Vector v, void *el, int(*cmp)(const void *a, const void *b));
+
+/**
+ * @brief Search with O(logN) complexity an element into the vector
+ * @param v 
+ * @param el 
+ * @param cmp 
+ * @return Index of the element or vector_size(v) otherwise
+ * @pre v != NULL && el != NULL && v is sorted (with cmp function)
+ * @post idx == vector_size(v) || cmp(vector_element(v, idx), el) == 0
+ */
+unsigned int vector_sorted_search(Vector v, void *el, int(*cmp)(const void *a, const void *b));
+
+// ------------------------ MODIFICATIVE FUNCTIONS ------------------------
+
+/**
+ * @brief Assign element el to position idx of the vector (the structure use a copy of the element)
+ * @param v 
+ * @param el 
+ * @param idx 
+ * @pre v != NULL && el != NULL && idx < vector_size(v)
+ * @post vector_element(v, idx) == el
+ */
+void vector_assign(Vector v, void *el, unsigned int idx);
+
+// ------------------------ ERASE FUNCTIONS ------------------------
+
+/**
+ * @brief Erase an element from the end of the vector
+ * @param v 
+ * @pre v != NULL && !vector_is_empty(v)
+ */
+void vector_pop_back(Vector v);
+
+/**
+ * @brief Erase with O(N) complexity an element in position idx of the vector
+ * @param v 
+ * @param idx 
+ * @pre v != NULL && !vector_is_empty(v) && idx < vector_size(v)
+ */
+void vector_erase(Vector v, unsigned int idx);
+
+// ------------------------ SORT FUNCTIONS ------------------------
+
+/**
+ * @brief Sort with O(NlogN) complexity the vector
+ * @param v 
+ * @param cmp 
+ * @pre v != NULL
+ */
+void vector_merge_sort(Vector v, int(*cmp)(const void *a, const void *b));
+
+#endif
