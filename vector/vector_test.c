fe36d79d39688b416d3787f592f06820cd71fae3
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-03-26 03:53:28 -0300

    Algunos tests sobre cómo funciona y se comporta vector

--- vector/vector_test.c
+++ vector/vector_test.c
@@ -5,288 +5,154 @@
 
 // ------------------------ TEST 1 ------------------------
 
+bool test1_ordenado = false;
+
 int cmp_test1(const void *a, const void *b) {
-  int A = *(int *)a, B = *(int *)b;
-  if (A < B)
-    return -1;
-  else if (A == B)
-    return 0;
-  return 1;
+  return *((int *)a) - *((int *)b);
 }
 
-void print_vector_test1(Vector v, int b) {
-  printf("Size -> %lu\nIsEmpty -> %u\nElements:\n   {", vector_size(v),
-         vector_is_empty(v));
-  for (unsigned int i = 0; i < vector_size(v); i++) {
-    if (i != 0) printf(", ");
-    int *x = (int *)vector_element(v, i);
-    printf("%d", (*x));
-    free(x);
+void vector_print_test1(Vector v) {
+  size_t sz = vector_tamano(v);
+  printf("Tamaño -> %ld\nEstá vacío? -> %d\nElementos:\n{ ", sz,
+         vector_esta_vacio(v));
+  for (unsigned int i = 0; i < sz; i++) {
+    if (i) printf(", ");
+
+    int *x = vector_elemento(v, i);
+    printf("%d", *x);
   }
-  printf("}\n-----------------------------------------------------\n");
-  if (b == 0)
-    printf("Simple search\n{");
+  if (test1_ordenado)
+    printf(" }\nOrdenado:\n{ ");
   else
-    printf("Ordered search\n{");
-  for (unsigned int i = 0; i < vector_size(v); i++) {
-    if (i != 0) printf(", ");
-    int *x = (int *)vector_element(v, i);
-    if (b == 0)
-      printf("%u", vector_search(v, x, cmp_test1));
+    printf(" }\nLineal:\n{ ");
+  for (unsigned int i = 0; i < sz; i++) {
+    if (i) printf(", ");
+
+    int *x = vector_elemento(v, i);
+    if (test1_ordenado)
+      printf("%u", vector_busqueda_ordenada(v, x, cmp_test1));
     else
-      printf("%u", vector_sorted_search(v, x, cmp_test1));
-    free(x);
+      printf("%u", vector_busqueda(v, x, cmp_test1));
   }
-  printf(
-      "}\n=================================================================="
-      "\n");
+  printf(" }\n------------------------- \n");
 }
 
 void test1() {
-  printf(
-      "}\n====================================== TEST 1 "
-      "============================"
-      "\n");
+  printf(" ======================= TEST1 ======================= \n");
   Vector v = NULL;
-  v = vector_initialize(v);
+  v = vector_inicializar(v);
 
-  int arr0[10] = {-1, 5, 3, 7, -1435, 134364745, 0, 0, 1, 3};
+  int lista_test1[10] = {9, 8, 7, 1, 2, 3, 5, 6, 4, 0};
   for (unsigned int i = 0; i < 10; i++) {
     int *x = malloc(sizeof(int));
-    *x = arr0[i];
-    vector_push_back(v, (void *)x);
-    free(x);
+    *x = lista_test1[i];
+    vector_encolar(v, x);
   }
-  print_vector_test1(v, 0);
+  vector_print_test1(v);
 
-  int *y = malloc(sizeof(int));
-  *y = 1500000;
-  vector_assign(v, y, 0);
-  free(y);
-  print_vector_test1(v, 0);
+  int *x0 = malloc(sizeof(int));
+  *x0 = -100;
+  vector_asignar(v, x0, 0);
+  vector_print_test1(v);
 
-  vector_pop_back(v);
-  print_vector_test1(v, 0);
+  vector_eliminar(v, 2);
+  vector_print_test1(v);
 
-  vector_erase(v, 2);
-  print_vector_test1(v, 0);
+  vector_eliminar_ultimo(v);
+  vector_print_test1(v);
 
-  vector_erase(v, 0);
-  print_vector_test1(v, 0);
+  vector_ordenar(v, cmp_test1);
+  test1_ordenado = true;
+  vector_print_test1(v);
 
-  vector_erase(v, vector_size(v) - 1);
-  print_vector_test1(v, 0);
-
-  for (unsigned int i = 0; i < 10; i++) {
-    int *x = malloc(sizeof(int));
-    *x = arr0[i];
-    vector_push_back(v, (void *)x);
-    free(x);
-  }
-  print_vector_test1(v, 0);
-
-  while (vector_size(v) > 3) vector_pop_back(v);
-  print_vector_test1(v, 0);
-
-  vector_merge_sort(v, cmp_test1);
-  print_vector_test1(v, 1);
-
-  vector_free(v);
+  vector_liberar(v);
 }
 
 // ------------------------ TEST 2 ------------------------
 
-typedef struct PairSt {
+bool test2_ordenado = false;
+
+struct NodoSt {
   int x, y;
-} pair;
+  Vector lista;
+};
+typedef struct NodoSt *Nodo;
 
 int cmp_test2(const void *a, const void *b) {
-  pair *A = (pair *)a, *B = (pair *)b;
-  int *Ax = malloc(sizeof(int));
-  *Ax = (*A).x;
-  int *Ay = malloc(sizeof(int));
-  *Ay = (*A).y;
-  int *Bx = malloc(sizeof(int));
-  *Bx = (*B).x;
-  int *By = malloc(sizeof(int));
-  *By = (*B).y;
-
-  int cmp1 = cmp_test1((void *)Ax, (void *)Bx),
-      cmp2 = cmp_test1((void *)Ay, (void *)By);
-
-  free(Ax);
-  free(Ay);
-  free(Bx);
-  free(By);
-
-  if (cmp1 == 0)
-    return cmp2;
-  else
-    return cmp1;
+  return (*((Nodo)a)).y - (*((Nodo)b)).y;
 }
 
-void print_vector_test2(Vector v, int b) {
-  printf("Size -> %lu\nIsEmpty -> %u\nElements:\n   {", vector_size(v),
-         vector_is_empty(v));
-  for (unsigned int i = 0; i < vector_size(v); i++) {
-    if (i != 0) printf(", ");
-    pair *x = (pair *)vector_element(v, i);
-    printf("{%d, %d}", (*x).x, (*x).y);
-    free(x);
+void vector_print_test2(Vector v) {
+  size_t sz = vector_tamano(v);
+  printf("Tamaño -> %ld\nEstá vacío? -> %d\nElementos:\n{ ", sz,
+         vector_esta_vacio(v));
+  for (unsigned int i = 0; i < sz; i++) {
+    if (i) printf(", ");
+
+    Nodo x = vector_elemento(v, i);
+    printf("{ %d, %d, \n[\n", x->x, x->y);
+
+    vector_print_test1(x->lista);
+    printf(" ]\n");
   }
-  printf("}\n-----------------------------------------------------\n");
-  if (b == 0)
-    printf("Simple search\n{");
-  else
-    printf("Ordered search\n{");
-  for (unsigned int i = 0; i < vector_size(v); i++) {
-    if (i != 0) printf(", ");
-    pair *x = (pair *)vector_element(v, i);
-    if (b == 0)
-      printf("%u", vector_search(v, x, cmp_test2));
-    else
-      printf("%u", vector_sorted_search(v, x, cmp_test2));
-    free(x);
-  }
-  printf(
-      "}\n=================================================================="
-      "\n");
+  printf(" }\n------------------------- \n\n\n");
 }
 
 void test2() {
-  printf(
-      "}\n====================================== TEST 2 "
-      "============================"
-      "\n");
+  printf(" ======================= TEST2 ======================= \n");
   Vector v = NULL;
-  v = vector_initialize(v);
+  v = vector_inicializar(v);
 
-  pair arr0[10] = {{-1, 0},        {-1435, 1}, {3, 2}, {7, -99999}, {-1435, 4},
-                   {134364745, 5}, {0, 6},     {0, 7}, {1, 8},      {3, 9}};
+  struct NodoSt lista_test1[10] = {
+      {1002, 9, NULL}, {-1, 8, NULL}, {-1, 7, NULL}, {-1, 1, NULL},
+      {-1, 2, NULL},   {-1, 3, NULL}, {-1, 5, NULL}, {-1, 6, NULL},
+      {0, 4, NULL},    {0, 0, NULL}};
   for (unsigned int i = 0; i < 10; i++) {
-    pair *x = malloc(sizeof(pair));
-    *x = arr0[i];
-    vector_push_back(v, (void *)x);
-    free(x);
+    Nodo x = malloc(sizeof(struct NodoSt));
+    *x = lista_test1[i];
+    x->lista = NULL;
+    x->lista = vector_inicializar(x->lista);
+    vector_encolar(v, x);
   }
-  print_vector_test2(v, 0);
+  vector_print_test2(v);
 
-  pair *y = malloc(sizeof(pair));
-  (*y).x = 1500000;
-  (*y).y = -123;
-  vector_assign(v, y, 0);
-  free(y);
-  print_vector_test2(v, 0);
-
-  vector_pop_back(v);
-  print_vector_test2(v, 0);
-
-  vector_erase(v, 2);
-  print_vector_test2(v, 0);
-
-  vector_erase(v, 0);
-  print_vector_test2(v, 0);
-
-  vector_erase(v, vector_size(v) - 1);
-  print_vector_test2(v, 0);
-
-  for (unsigned int i = 0; i < 10; i++) {
-    pair *x = malloc(sizeof(pair));
-    *x = arr0[i];
-    vector_push_back(v, (void *)x);
-    free(x);
+  Nodo x0 = malloc(sizeof(struct NodoSt));
+  x0->x = 12445435;
+  x0->y = -14335;
+  x0->lista = NULL;
+  x0->lista = vector_inicializar(x0->lista);
+  for (unsigned int i = 0; i < 10; i += 2) {
+    int *x = malloc(sizeof(int));
+    *x = i;
+    vector_encolar(x0->lista, x);
   }
-  print_vector_test2(v, 0);
+  Nodo x1 = vector_elemento(v, 0);
+  vector_liberar(x1->lista);
+  vector_asignar(v, x0, 0);
+  vector_print_test2(v);
 
-  while (vector_size(v) > 3) vector_pop_back(v);
-  print_vector_test2(v, 0);
+  x1 = vector_elemento(v, 2);
+  vector_liberar(x1->lista);
+  vector_eliminar(v, 2);
+  vector_print_test2(v);
 
-  vector_merge_sort(v, cmp_test1);
-  print_vector_test2(v, 1);
+  x1 = vector_elemento(v, vector_tamano(v) - 1);
+  vector_liberar(x1->lista);
+  vector_eliminar_ultimo(v);
+  vector_print_test2(v);
 
-  vector_free(v);
-}
+  vector_ordenar(v, cmp_test2);
+  vector_print_test2(v);
 
-// ------------------------ TEST 2 ------------------------
-
-typedef struct SomeSt {
-  int x, y;
-} Some;
-
-void print_vector_test3(Vector v) {
-  printf("Size -> %lu\nIsEmpty -> %u\nElements:\n   {", vector_size(v),
-         vector_is_empty(v));
-  for (unsigned int i = 0; i < vector_size(v); i++) {
-    if (i != 0) printf(", ");
-    Some *x = (Some *)vector_element(v, i);
-    printf("{%d, %d}", (*x).x, (*x).y);
-    free(x);
+  for (unsigned int i = 0; i < vector_tamano(v); i++) {
+    Nodo x = vector_elemento(v, i);
+    vector_liberar(x->lista);
   }
-  printf(
-      "}\n=================================================================="
-      "\n");
-}
-
-void test3() {
-  printf(
-      "}\n====================================== TEST 3 "
-      "============================"
-      "\n");
-
-  Vector v = NULL;
-  Some arr0[10] = {{-1, 0, 1},    {-1435, 1, 1},     {3, 2, 1}, {7, -99999, 1},
-                   {-1435, 4, 1}, {134364745, 5, 1}, {0, 6, 1}, {0, 7, 1},
-                   {1, 8, 1},     {3, 9, 1}};
-  Some *z = malloc(sizeof(Some));
-  *z = arr0[0];
-  v = vector_initialize_with_elements(v, 10, z);
-  free(z);
-
-  print_vector_test3(v);
-
-  for (unsigned int i = 0; i < 10; i++) {
-    Some *x = malloc(sizeof(Some));
-    *x = arr0[i];
-    vector_push_back(v, (void *)x);
-    free(x);
-  }
-  print_vector_test3(v);
-
-  Some *y = malloc(sizeof(Some));
-  (*y).x = 1500000;
-  (*y).y = -123;
-  vector_assign(v, y, 0);
-  free(y);
-  print_vector_test3(v);
-
-  vector_pop_back(v);
-  print_vector_test3(v);
-
-  vector_erase(v, 2);
-  print_vector_test3(v);
-
-  vector_erase(v, 0);
-  print_vector_test3(v);
-
-  vector_erase(v, vector_size(v) - 1);
-  print_vector_test3(v);
-
-  for (unsigned int i = 0; i < 10; i++) {
-    Some *x = malloc(sizeof(Some));
-    *x = arr0[i];
-    vector_push_back(v, (void *)x);
-    free(x);
-  }
-  print_vector_test3(v);
-
-  while (vector_size(v) > 3) vector_pop_back(v);
-  print_vector_test3(v);
-
-  vector_free(v);
+  vector_liberar(v);
 }
 
 int main() {
   test1();
+  test1_ordenado = false;
   test2();
-  test3();
 }
