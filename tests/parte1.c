ab5ef2cae8ef7038c5e68b90139081a9abae5efc
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-03-27 17:59:36 -0300

    Hecho todos los tests y chequeado que ande bien

--- tests/parte1.c
+++ tests/parte1.c
@@ -9,19 +9,19 @@ void grafo_print(Grafo g) {
   printf("Cantidad de nodos: %u\nCantidad de aristas: %u\nDelta: %u\nNodos:\n",
          n, NumeroDeLados(g), Delta(g));
 
-  for (u32 i = 0; i < n; i++) {
-    printf("    Nombre: %u\n", Nombre(i, g));
+  // for (u32 i = 0; i < n; i++) {
+  //   printf("    Nombre: %u\n", Nombre(i, g));
 
-    u32 sz = Grado(i, g);
-    printf("    Grado: %u\nVecinos: ", sz);
+  //   u32 sz = Grado(i, g);
+  //   printf("    Grado: %u\nVecinos: ", sz);
 
-    for (u32 j = 0; j < sz; j++) {
-      u32 x = IndiceVecino(j, i, g);
-      u32 y = Nombre(x, g);
-      printf("(%u, %u) ", x, y);
-    }
-    printf("\n================\n");
-  }
+  //   for (u32 j = 0; j < sz; j++) {
+  //     u32 x = IndiceVecino(j, i, g);
+  //     u32 y = Nombre(x, g);
+  //     printf("(%u, %u) ", x, y);
+  //   }
+  //   printf("\n================\n");
+  // }
 }
 
 int main(void) {
