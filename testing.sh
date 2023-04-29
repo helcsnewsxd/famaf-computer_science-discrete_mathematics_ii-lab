9974b5b1b42ae51166a7921d7f4e0a20bc32b05e
Emanuel Nicolas Herrador <emanuel.nicolas.herrador@unc.edu.ar>
2023-04-29 17:17:46 -0300

    Agregado script para testeo

---
+++ testing.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+gcc -Wall -Wextra -std=c99 -O3 main.c coloreo/APIParte2.c Implementacion_grupo_gonza_APIG.h/APIG23.c -o ej
+for i in grafos_de_ejemplo/*.txt
+do
+  echo $i
+  time ./ej < $i
+  echo "---------------"
+done
+rm ej
