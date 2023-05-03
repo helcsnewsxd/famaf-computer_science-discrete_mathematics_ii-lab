7a4435129abeb3672f9ddd2bd850f2b613eeae1f
helcsnewsxd <emanuel.nicolas.herrador@unc.edu.ar>
2023-05-03 10:35:59 -0300

    Fixeado test para cualquier archivo de grafo

--- testing.sh
+++ testing.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 gcc -Wall -Wextra -std=c99 -O3 main.c coloreo/APIParte2.c Implementacion_grupo_gonza_APIG.h/APIG23.c -o ej
-for i in grafos_de_ejemplo/*.txt
+for i in grafos_de_ejemplo/*
 do
   echo $i
   time ./ej < $i
